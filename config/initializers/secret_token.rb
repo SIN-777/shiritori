3cf032ef168bc87e6b78fe777099dbf8bc4a7937
zizochan <zizochan@gmail.com>
2010-10-08 12:28:56 +0900

    base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shiritori::Application.config.secret_token = '3a5ba5cc5c435f54fd14f51f2248232bcd29e8427ca89779073b86fb3b356e52ce3e53abe05d366a696cb3a9e5c2399cbfce4c87dafe52a5d7859966b49aa87c'
