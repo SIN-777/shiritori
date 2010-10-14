15d383d7aba94a3a985c0ce499e14900a9487766
zizochan <zizochan@gmail.com>
2010-10-14 12:10:52 +0900

    メソッド整理

--- app/controllers/shiritori_controller.rb
+++ app/controllers/shiritori_controller.rb
@@ -1,13 +1,5 @@
 class ShiritoriController < ApplicationController
 
-  def get_new_question
-    RubyMethod.all.sort_by{|m| rand}.first
-  end
-
-  def get_next_question(answer)
-    RubyMethod.find_all_by_initial(answer[answer.length - 1, 1]).sort_by{|m| rand}.first
-  end
-
   def check_answer(question, answer)
     if question.terminal != answer[0, 1]
       flash[:ng_message] = "しりとりがつながりません"
@@ -27,12 +19,12 @@ public
     if request[:question_id] && request[:answer]
       before_question = RubyMethod.find(request[:question_id])
       if check_answer(before_question, params[:answer])
-        @question = get_next_question(params[:answer])
+        @question = RubyMethod.get_next_question(params[:answer])
       else
         @question = before_question
       end
     else
-      @question = get_new_question
+      @question = RubyMethod.get_random
     end
   end
 
