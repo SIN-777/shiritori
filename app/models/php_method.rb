14e41243afd5badc9fa27226004f49322020d433
zizochan <zizochan@gmail.com>
2010-10-15 12:30:43 +0900

    add php methods

---
+++ app/models/php_method.rb
@@ -0,0 +1,2 @@
+class PhpMethod < ActiveRecord::Base
+end
---
+++ test/unit/php_method_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class PhpMethodTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
