7636b68fbfb83af2a1eb4c81b237832e69dafe4e
zizochan <zizochan@gmail.com>
2010-10-20 22:03:33 +0900

    add ruby methods

--- app/models/language_method.rb
+++ app/models/language_method.rb
@@ -2,14 +2,6 @@ class LanguageMethod < ActiveRecord::Base
   belongs_to :language
 
 public
-  def self.get_random
-    self.all.sort_by{|m| rand}.first
-  end
-
-  def self.get_next_question(answer)
-    self.find_all_by_initial(answer[answer.length - 1, 1]).sort_by{|m| rand}.first
-  end
-
   def terminal
     body[body.length - 1 , 1]
   end
